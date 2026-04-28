69975f46f0765696cd93cbb3e44a53442bf17d0c
MasonTheBag <masonthebag@gmail.com>
2026-04-28 08:33:30 -0400

    player animation stuff

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Animator animator;
+
+    
     void Start()
     {
         
+        animator = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+
+        if (animator != null)
+        {
+
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+
+                animator.SetTrigger("Jump");
+
+            }
+
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+
+                // lockpick
+
+            }
+
+
+        }
+
     }
 }
